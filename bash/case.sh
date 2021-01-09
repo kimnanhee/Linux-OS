3f13d865653c9bcae0a6947caa0bbc21e9f94ab3
kimnanhee <kimnanhee0225@gmail.com>
2021-01-09 15:43:56 +0900

    [Modify] case.sh

--- bash/case.sh
+++ bash/case.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-if [ "woo" = "woo" ]
-then
-	echo "참입니다"
-else
-	echo "거짓입니다"
-fi
+echo "리눅스가 좋나요? (y/n)"
+read answer
+case $answer in
+	yes | y | Y | Yes | YES)
+		echo "좋습니다!";;
+	[nN]*)
+		echo "슬퍼요ㅠ";;
+	*)
+		echo "잘못된 입력입니다."
+		exit 1;;
+esac
 
 exit 0
